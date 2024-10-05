4a613a40cc5f4a7bc71cb0850d7ac55c4fc33510
MartinLicciardello <martinlicciardello7@gmail.com>
2024-10-05 23:04:50 +0000

    punto d

---
+++ puntod.sh
@@ -0,0 +1,6 @@
+96  mkdir -p EstructuraAsimetrica/{{correo,cliente}/cartas{1..100},correo/carteros_{1..10}}
+   97  tree Estructura_Asimetrica/
+   98  ll
+   99  tree EstructuraAsimetrica/
+  100  history
+
