55bec28e13ea072ba56df95f484680595e34a4ae
MartinLicciardello <martinlicciardello7@gmail.com>
2024-10-05 21:21:09 +0000

    Ejercicio 1 y 2

---
+++ puntoa.sh
@@ -0,0 +1,1 @@
+sudo mkdir -p Examenes-UTN/{profesores,alumno{1..3}/parcial{1..3}}
---
+++ puntob.sh
@@ -0,0 +1,19 @@
+29  sudo fdisk /dev/sdc
+   30  lsblk
+   31  sudo mkfs.ext4 /dev/sdc1
+   32  sudo mkfs.ext4 /dev/sdc2
+   33  sudo mkfs.ext4 /dev/sdc3
+   34  sudo mkfs.ext4 /dev/sdc4
+   35  sudo mkdir /mnt/sdc1
+   36  sudo mount /dev/sdc1 /mnt/sdc1
+   37  ll /mnt/sdc1/
+   38  ll /mnt/
+   39  sudo mkdir /mnt/sdc2
+   40  sudo mount /dev/sdc2 /mnt/sdc2
+   41  sudo mkdir /mnt/sdc3
+   42  sudo mount /dev/sdc3 /mnt/sdc3
+   43  sudo mkdir /mnt/sdc4
+   44  sudo mount /dev/sdc4 /mnt/sdc4
+   45  ll /mnt/
+   46  history
+
