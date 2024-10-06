9a5eac5446cdec7d3164968f1ca30bdf460a0362
MartinLicciardello <martinlicciardello7@gmail.com>
2024-10-06 02:56:59 +0000

    punto f resuelto

--- puntof.sh
+++ puntof.sh
@@ -1,1 +1,8 @@
+ 118  curl -s ifconfig.me > filtroAvanzado.txt
+  119  whoami >> filtroAvanzado.txt
+  120  sudo cat /etc/shadow |grep vagrant |awk -F ':' '{print $2}' >> filtroAvanzado.txt
+  121  cat filtroAvanzado.txt
+  122  nano filtroAvanzado.txt
+  123  cat filtroAvanzado.txt
+  124  history
 ll
